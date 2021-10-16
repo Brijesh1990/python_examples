462e520897c31c05e030e143c76234d4d70eee7e
Brijesh1990 <bkpandey.pandey@gmail.com>
2021-10-16 23:29:24 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+# list is a just like to simliar as array where we store a data in list using [] and store multiple data using (,)
+# list is a mutable datatypes of sequesnce tyeps in python
+
+
+emp=["vaishali","brijesh","meet","jayesh","kumar"]
+print(emp)
+print(type(emp))
+print(emp[0:3])
+print(emp[0:2])
+print(emp+emp)
+print(emp*4)
